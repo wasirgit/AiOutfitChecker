a77707d74dd6a0ac5f5e73efee1fb4edacbe91f0
Wasir <live.wasir@gmail.com>
2026-02-04 19:29:58 +0600

    Initial Commit

---
+++ aioutfitchecker/aioutfitcheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  aioutfitcheckerApp.swift
+//  aioutfitchecker
+//
+//  Created by Israt Jahan Simu on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct aioutfitcheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
